36c20a9ec5a39f00b4ef35dfcf7478681599fb39
William Okou <gokou097@gmail.com>
2023-07-06 15:39:35 +0100

    added entry to portfolio

--- app/data/portfolio.js
+++ app/data/portfolio.js
@@ -6,9 +6,15 @@ export default [
     link: 'https://gokou.vercel.app',
   },
   {
-    title: 'Expense Tracker (Work in Progress)',
+    title: 'Expense Tracker',
     imgUrl: '',
     stack: ['Typescript', 'Tailwind', 'NextJS', 'tRPC', 'Prisma', 'PostgreSQL'],
     link: 'https://goexpenses.vercel.app',
+  },
+  {
+    title: 'Web3 DApp',
+    imgUrl: '',
+    stack: ['Typescript', 'Tailwind', 'NextJS', 'Moralis.io'],
+    link: 'moralis-app-five.vercel.app',
   },
 ];
