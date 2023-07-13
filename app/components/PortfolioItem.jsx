273af76a1f0008379f25ae92d39ea290ec3bf68f
William Okou <gokou097@gmail.com>
2023-07-13 02:44:09 +0100

    New portfolio item added

--- app/components/PortfolioItem.jsx
+++ app/components/PortfolioItem.jsx
@@ -6,24 +6,25 @@ const PortfolioItem = ({title, imgUrl, stack, link}) => {
   return (
     <Link
       href={link}
-      target='_blank'
-      rel='noopener noreferrer'
-      className='border-2 border-stone-900 dark:border-white rounded-md overflow-hidden'
+      target="_blank"
+      rel="noopener noreferrer"
+      className="border-2 border-stone-900 dark:border-white rounded-md overflow-hidden"
     >
       <Image
         src={imgUrl}
-        alt='portfolio'
-        className='w-full h-36 md:h-48 object-cover cursor-pointer'
+        alt="PortfolioItem"
+        className="w-full h-36 md:h-48 object-cover cursor-pointer"
       />
-      <div className='w-full p-4'>
-        <h3 className='text-lg md:text-xl mb-2 md:mb-3 font-semibold dark:text-white'>
+
+      <div className="w-full p-4">
+        <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold dark:text-white">
           {title}
         </h3>
-        <p className='flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm'>
+        <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
           {stack.map((item) => (
             <span
               key={item}
-              className='inline-block px-2 py-1 font-semibold border-2 border-stone-900 dark:border-white rounded-md dark:text-white'
+              className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 dark:border-white rounded-md dark:text-white"
             >
               {item}
             </span>
